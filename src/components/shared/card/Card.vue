43b0e7becc2721c0946212f0a097e9fa571f7fcc
lmmendonca <lmaximino.mendonca@gmail.com>
2019-03-28 20:59:06 -0300

    Alteracao do card

--- src/components/shared/card/Card.vue
+++ src/components/shared/card/Card.vue
@@ -1,41 +1,50 @@
 <template>
   <md-card class="hbt-card">
     <md-card-content>
-      <table class="table-card" width="100%" align="block">
-        <tr>
-          <th class="col-1"></th>
-          <th class="col-2"></th>
-          <th class="col-3"></th>
-        </tr>
-        <tr>
-          <td class="lin-1">
-            <img src="./../../../assets/icons/card/cliente.png">
-            <span>{{ nome }}</span>
-          </td>
-          <td class="lin-1">
-            <img src="./../../../assets/icons/card/pin.png">
-            <span>{{ empreendimento }}</span>
-          </td>
-          <td class="lin-1">
-            <img src="./../../../assets/icons/card/status.png">
-            <span>{{ status_negocio }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td class="lin-2">
-            <img src="./../../../assets/icons/card/documento.png">
-            <span>{{ cpf }}</span>
-          </td>
-          <td class="lin-2">
-            <img src="./../../../assets/icons/card/imovel.png">
-            <span>{{ imovel }}</span>
-          </td>
-          <td class="lin-2">
-            <img src="./../../../assets/icons/card/data-compra.png">
-            <span>{{ data_compra }}</span>
-          </td>
-        </tr>
-      </table>
+      <div class="md-layout">
+        <div class="md-layout-item">
+          <div class="md-layout lin-1">
+            <div class="md-layout-item">
+              <img src="./../../../assets/icons/card/cliente.png">
+              {{nome}}
+            </div>
+          </div>
+          <div class="md-layout">
+            <div class="md-layout-item">
+              <img src="./../../../assets/icons/card/documento.png">
+              {{cpf}}
+            </div>
+          </div>
+        </div>
+        <div class="md-layout-item">
+          <div class="md-layout lin-1">
+            <div class="md-layout-item">
+              <img src="./../../../assets/icons/card/pin.png">
+              {{empreendimento}}
+            </div>
+          </div>
+          <div class="md-layout">
+            <div class="md-layout-item">
+              <img src="./../../../assets/icons/card/imovel.png">
+              {{imovel}}
+            </div>
+          </div>
+        </div>
+        <div class="md-layout-item">
+          <div class="md-layout">
+            <div class="md-layout-item lin-1">
+              <img src="./../../../assets/icons/card/status.png">
+              {{status_negocio}}
+            </div>
+          </div>
+          <div class="md-layout">
+            <div class="md-layout-item">
+              <img src="./../../../assets/icons/card/data-compra.png">
+              {{data_compra}}
+            </div>
+          </div>
+        </div>
+      </div>
     </md-card-content>
   </md-card>
 </template>
